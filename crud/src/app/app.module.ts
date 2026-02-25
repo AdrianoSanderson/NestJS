01c16e7113deaeb43de4ffb3dc4a5888a3167356
Adriano Sanderson <adrianoramos06@gmail.com>
2026-02-24 22:22:19 -0300

    Configurando TypeOrm

--- crud/src/app/app.module.ts
+++ crud/src/app/app.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { MessagesModule } from 'src/messages/messages.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [MessagesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      database: 'postgres',
+      password: '12345',
+      autoLoadEntities: true, //Carrega entidades sem precisar especifica-las
+      synchronize: true, //Sinroniza com o BD. Não deve ser usado em produção.
+    }),
+    MessagesModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
